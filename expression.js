9888c2c23353b881f821d3d7000df4ddb9336864
Laurence R. Ugalde <laurence.ugalde@formulae.org>
2024-03-13 22:40:11 -0600

    conjugate style

--- expression.js
+++ expression.js
@@ -20,7 +20,59 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 export class Complex extends Formulae.ExpressionPackage {};
 
+Complex.conjugateStyle = 0; // 0: x*   1: upper line
+
+
+Complex.Conjugate = class extends Expression.SuperscriptedLiteral {
+	getTag() {
+		return "Math.Complex.Conjugate";
+	}
+	
+	getLiteral() {
+		return "*";
+	}
+	
+	getName() {
+		return Complex.messages.nameConjugate;
+	}
+	
+	prepareDisplay(context) {
+		if (Complex.conjugateStyle == 0) {
+			super.prepareDisplay(context);
+		}
+		else {
+			let child = this.children[0];
+			child.prepareDisplay(context);
+			
+			this.width = child.width;
+			this.height = 4 + child.height;
+			
+			this.horzBaseline = 4 + child.horzBaseline;
+			this.vertBaseline = child.vertBaseline;
+			
+			child.x = 0;
+			child.y = 4;
+		}
+	}
+	
+	display(context, x, y) {
+		if (Complex.conjugateStyle == 0) {
+			super.display(context, x, y);
+		}
+		else {
+			let child = this.children[0];
+			child.display(context, x + child.x, y + child.y);
+			
+			context.beginPath();
+			context.moveTo(x, y + 0.0); context.lineTo(x + this.width, y + 0.0); // preventing obfuscation
+			context.stroke();
+		}
+	}
+}
+
 Complex.setExpressions = function(module) {
+	Formulae.setExpression(module, "Math.Complex.Conjugate", Complex.Conjugate);
+	
 	// literals
 	[
 		[ "Math.Complex",  "Imaginary", "ℹ" ]
@@ -34,10 +86,52 @@ Complex.setExpressions = function(module) {
 	));
 	
 	// superscripted literals
+	/*
 	Formulae.setExpression(module, "Math.Complex.Conjugate", {
 		clazz:      Expression.SuperscriptedLiteral,
 		getTag:     () => "Math.Complex.Conjugate",
 		getLiteral: () => "*",
 		getName:    () => Complex.messages.nameConjugate
-	});
+	})
+	*/;
+};
+
+Complex.isConfigurable = () => true;
+
+Complex.onConfiguration = () => {
+	let table = document.createElement("table");
+	table.classList.add("bordered");
+	let row = table.insertRow();
+	let th = document.createElement("th"); th.setAttribute("colspan", "2"); th.appendChild(document.createTextNode(Complex.messages.labelComplex)); row.appendChild(th);
+	row = table.insertRow();
+	let col = row.insertCell();
+	col.appendChild(document.createTextNode(Complex.messages.labelConjugateStyle));
+	col = row.insertCell();
+	
+	let radio = document.createElement("input"); radio.type = "radio"; radio.addEventListener("click", () => Complex.onChangeConjugateStyle(0));
+	col.appendChild(radio);
+	
+	let span = document.createElement("span");
+	span.innerHTML = "x<sup>*</sup>";
+	col.appendChild(span);
+	
+	col.appendChild(document.createElement("br"));
+	col.appendChild(document.createElement("br"));
+		
+	radio = document.createElement("input"); radio.type = "radio"; radio.addEventListener("click", () => Complex.onChangeConjugateStyle(1));
+	col.appendChild(radio);
+	
+	span = document.createElement("span");
+	span.style = "text-decoration: overline;";
+	span.innerHTML = "x";
+	col.appendChild(span);
+	
+	Formulae.setModal(table);
+};
+
+Complex.onChangeConjugateStyle = function(pos) {
+	Formulae.resetModal();
+	
+	Complex.conjugateStyle = pos;
+	Formulae.refreshHandlers();
 };
